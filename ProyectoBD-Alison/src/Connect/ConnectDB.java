61f90bcd64d0b41ea5140ac896c56682ca8100ba
JP113 <JP113@PC113>
2024-04-17 18:36:47 -0600

    InsertPeople e InsertProduction, en script y en netbeans

--- ProyectoBD-Alison/src/Connect/ConnectDB.java
+++ ProyectoBD-Alison/src/Connect/ConnectDB.java
@@ -45,6 +45,74 @@ public class ConnectDB {
         stmt.execute();
         //insertClient(); // No se pasa ningún parámetro ya que el ID se genera automáticamente
     }
+    
+    public static void insertPerson(String firstName, String middleName, 
+                                    String lastName, String secondSurname, String biography, int height,
+                                    String trivia, int idDistrict) throws SQLException {
+        String host = "jdbc:oracle:thin:@localhost:1521:DBPrueba";
+        String uName = "proyectoDBA";
+        String uPass = "proyectoDBA";
+
+        Connection con = DriverManager.getConnection(host, uName, uPass);
+        CallableStatement stmt = con.prepareCall("{ call InsertUserSys(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) }");
+
+        stmt.setString(1, firstName);
+        stmt.setString(2, middleName);
+        stmt.setString(3, lastName);
+        stmt.setString(4, secondSurname);
+        stmt.setString(5, biography);
+        stmt.setNull(6, java.sql.Types.DATE); // p_Birthdate
+        stmt.setInt(8, height);
+        stmt.setNull(7, java.sql.Types.BLOB); // p_Photo
+        stmt.setString(8, trivia);
+        stmt.setInt(9, idDistrict);
+  
+        stmt.execute();
+     }
+    
+    public static void insertProduct(String firstName, String middleName, 
+                                    String lastName, String secondSurname, String biography, int height,
+                                    String trivia, int idDistrict) throws SQLException {
+        String host = "jdbc:oracle:thin:@localhost:1521:DBPrueba";
+        String uName = "proyectoDBA";
+        String uPass = "proyectoDBA";
+
+        Connection con = DriverManager.getConnection(host, uName, uPass);
+        CallableStatement stmt = con.prepareCall("{ call InsertUserSys(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) }");
+
+        stmt.setString(1, firstName);
+        stmt.setString(2, middleName);
+        stmt.setString(3, lastName);
+        stmt.setString(4, secondSurname);
+        stmt.setString(5, biography);
+        stmt.setNull(6, java.sql.Types.DATE); // p_Birthdate
+        stmt.setInt(7, height);
+        stmt.setNull(8, java.sql.Types.BLOB); // p_Photo
+        stmt.setString(9, trivia);
+        stmt.setInt(10, idDistrict);
+  
+        stmt.execute();
+     }
+
+    public static void insertProduction(int idCategory, String title, int duration, 
+                                        String synopsis, String trailer, int releaseYear) throws SQLException {
+        String host = "jdbc:oracle:thin:@localhost:1521:DBPrueba";
+        String uName = "proyectoDBA";
+        String uPass = "proyectoDBA";
+
+        Connection con = DriverManager.getConnection(host, uName, uPass);
+        CallableStatement stmt = con.prepareCall("{ call InsertProduction(?,?,?,?,?,?,?) }");
+
+        stmt.setInt(1, idCategory);
+        stmt.setString(2, title);
+        stmt.setInt(3, duration);
+        stmt.setString(4, synopsis);
+        stmt.setString(5, trailer);
+        stmt.setInt(6, releaseYear);
+        stmt.setNull(7, java.sql.Types.BLOB); // p_Photo
+
+        stmt.execute();
+    }
 
     /*public static void insertClient() throws SQLException {
         String host = "jdbc:oracle:thin:@localhost:1521:DBPrueba";
