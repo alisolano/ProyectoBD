f176c2455d9f9faed199393dcdd7312ab2bbfc97
alisolano <100251377+alisolano@users.noreply.github.com>
2024-04-21 19:12:00 -0600

    carpetas actualizadas
    subi todo ordenado en dos carpetas, una para el back y otro para el end

---
+++ ProyectoBD-Back/Categories.sql
@@ -0,0 +1,38 @@
+INSERT INTO category (id, name)
+VALUES (1, 'Accion');
+
+INSERT INTO category (id, name)
+VALUES (2, 'Aventuras');
+
+INSERT INTO category (id, name)
+VALUES (3, 'Ciencia Ficcion');
+
+INSERT INTO category (id, name)
+VALUES (4, 'Comedia');
+
+INSERT INTO category (id, name)
+VALUES (5, 'No-Ficcion');
+
+INSERT INTO category (id, name)
+VALUES (6, 'Drama');
+
+INSERT INTO category (id, name)
+VALUES (7, 'Fantas?a');
+
+INSERT INTO category (id, name)
+VALUES (8, 'Musical');
+
+INSERT INTO category (id, name)
+VALUES (9, 'Suspense');
+
+INSERT INTO category (id, name)
+VALUES (10, 'Terror');
+
+INSERT INTO category (id, name)
+VALUES (11, 'Documental');
+
+INSERT INTO category (id, name)
+VALUES (12, 'Romance');
+
+INSERT INTO category (id, name)
+VALUES (13, 'Ficcion');
