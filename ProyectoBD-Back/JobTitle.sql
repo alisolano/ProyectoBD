e457232d49c0b2ac583ca420e5a50ca9d5e4ff3a
alisolano <100251377+alisolano@users.noreply.github.com>
2024-04-22 22:18:05 -0600

    elimine carpetas

--- ProyectoBD-Back/JobTitle.sql
+++
@@ -1,7 +0,0 @@
-INSERT ALL
-INTO JobTitle (id, Title) VALUES (1, 'Actor/Actriz')
-INTO JobTitle (id, Title) VALUES (2, 'Director')
-INTO JobTitle (id, Title) VALUES (3, 'Productor')
-INTO JobTitle (id, Title) VALUES (4, 'Otros')
-SELECT 1 FROM dual;
-COMMIT;
